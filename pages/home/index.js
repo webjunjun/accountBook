0f01400d53997cf695ecea9006cc5ea579dccbab
webjunjun <meliujun@qq.com>
2020-06-08 17:13:13 +0800

    添加图表组件

--- pages/home/index.js
+++ pages/home/index.js
@@ -6,7 +6,37 @@ Page({
             type: 'income'
         }, {
             type: 'cost'
-        }]
+        }],
+        onInitChart(F2, config) {
+            F2.Global.fontFamily = 'sans-serif';
+            const chart = new F2.Chart(config);
+            const data = [
+                { value: 63.4, city: 'New York', date: '2011-10-01' },
+                { value: 62.7, city: 'Alaska', date: '2011-10-01' },
+                { value: 72.2, city: 'Austin', date: '2011-10-01' },
+                { value: 58, city: 'New York', date: '2011-10-02' },
+                { value: 59.9, city: 'Alaska', date: '2011-10-02' },
+                { value: 67.7, city: 'Austin', date: '2011-10-02' },
+                { value: 53.3, city: 'New York', date: '2011-10-03' },
+                { value: 59.1, city: 'Alaska', date: '2011-10-03' },
+                { value: 69.4, city: 'Austin', date: '2011-10-03' },
+            ];
+            chart.source(data, {
+                date: {
+                    range: [0, 1],
+                    type: 'timeCat',
+                    mask: 'MM-DD'
+                },
+                value: {
+                    max: 300,
+                    tickCount: 4
+                }
+            });
+            chart.area().position('date*value').color('city').adjust('stack');
+            chart.line().position('date*value').color('city').adjust('stack');
+            chart.render();
+            return chart;
+        }
     },
     onLoad: function(options) {
         // 页面创建时执行
